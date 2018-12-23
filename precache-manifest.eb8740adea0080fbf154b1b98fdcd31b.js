3bf2c293834bcddf26d31aaa5bc74505c789446f
Ronen Barel <proronen@gmail.com>
2018-12-23 11:32:07 +0200

    Herolo gitpages commit

---
+++ precache-manifest.eb8740adea0080fbf154b1b98fdcd31b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1502a895536fe15e844a",
+    "url": "/static/js/main.1502a895.chunk.js"
+  },
+  {
+    "revision": "039755a25906cde8f0f1",
+    "url": "/static/js/1.039755a2.chunk.js"
+  },
+  {
+    "revision": "1502a895536fe15e844a",
+    "url": "/static/css/main.88d38ac1.chunk.css"
+  },
+  {
+    "revision": "eb6ded01b1233cd34761e7f26cddb47e",
+    "url": "/index.html"
+  }
+];
